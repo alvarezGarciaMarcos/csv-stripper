d49e79b82baf3823f56b647f2b8c22982aee101e
Marcos Alvarez <marcosalvarez@MacBook-Pro.home>
2020-11-09 19:26:19 +0100

    Added the relase to the setup.py

--- setup.py
+++ setup.py
@@ -15,4 +15,7 @@ setup(
         [console_scripts]
         stripper=stripper:cli
     ''',
+    url='https://github.com/alvarezGarciaMarcos/csv-stripper',
+    download_url="https://github.com/alvarezGarciaMarcos/csv-stripper/archive/1.0.tar.gz"
+
 )
